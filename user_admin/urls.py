dc07866d05a3620751020bb1da3e3ec2b704556d
Juan <Arismendy>
2021-09-28 22:15:45 -0500

    making urls on the app

---
+++ user_admin/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # api version 1
+    #path('index', views.index, name='index_view'),
+    # path('api/v1/donors', views.DonorsView.as_view(), name='donor_view'),# general donor - POST, GET methods implemented
+
+]
